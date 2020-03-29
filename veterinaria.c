2b6ce9df6ba911f3dc260c35ef56608d9000f5a9
dbayonac <dbayonac@unal.edu.co>
2020-03-29 16:53:33 -0500

    LL Implemented

--- veterinaria.c
+++ veterinaria.c
@@ -8,16 +8,18 @@
 #define TamanoVeterinaria 1000000L
 
 struct dogType{
-    long ID;
     char nombre[32];
     char tipo[32];
     long edad;
     char raza[16];
     long estatura;
     float peso;
-    char sexo[1];
-    int initialized;
+    char sexo[2];
+    short initialized;
+    long next;
+    long prev;
 };
+
 
 struct dogType Veterinaria[(2*TamanoVeterinaria)];
 
@@ -43,21 +45,24 @@ void escribir(struct dogType *a){
 
 
 void veterinaria(){
-    FILE *fpnombres;
-    fpnombres = fopen("Nombre.txt","r");
+   FILE *fpnombres;
+    fpnombres = fopen("nombres.txt","r");
     bool colision = true;
     char nombre[32];
     for(long x = 0; x<(2*TamanoVeterinaria);x++){
         Veterinaria[x].initialized = 0;
+        Veterinaria[x].next = -1;
+        Veterinaria[x].prev = -1;
     }
     for(long i = 0; i < TamanoVeterinaria ; i++){
         colision = true;
         if(i%1700 == 0)fseek(fpnombres, 0, SEEK_SET);
         fgets(nombre,sizeof(nombre),fpnombres);
         long index = HashFunction(nombre);
-        printf("%li \n",i);
-        index+=(int)(i/1700);
-        index%=(2*TamanoVeterinaria);
+        while(Veterinaria[index].next != -1){
+            index = Veterinaria[index].next;
+        }
+        unsigned long previous = index;
         do{
             if(Veterinaria[index].initialized == 1){
             index = (index+1)%(2*TamanoVeterinaria);
@@ -66,21 +71,25 @@ void veterinaria(){
             else colision=false;
         }while(colision);
         Veterinaria[index].initialized=1;
-        Veterinaria[index].ID=index;
+        if(previous != index){
+            Veterinaria[index].prev = previous;
+            Veterinaria[previous].next = index;
+        }
         strcpy(Veterinaria[index].nombre, nombre);
         strcpy(Veterinaria[index].tipo, "perro");
         Veterinaria[index].edad=2*i % 14;
-        strcpy(Veterinaria[index].raza, "Golden Retriever");
+        strcpy(Veterinaria[index].raza, "Golden");
         Veterinaria[index].estatura = 3*i+i % 100;
         Veterinaria[index].peso = (2.3 * i + i % 50) + 10;
         if(i %2 == 0) strcpy(Veterinaria[index].sexo, "H");
         else strcpy(Veterinaria[index].sexo, "M");
-        //printf("%li\n",i);
+        printf("%li\n",i);
         //printf("index: %li , %i \n",index,Veterinaria[index].initialized);
     }
     //for(long x = 0; x<(TamanoVeterinaria);x++)printf("%i",Veterinaria[x].initialized);
     
     escribir(Veterinaria);
     fclose(fpnombres);
+    return 0;
     
 }
--- veterinaria.h
+++ veterinaria.h
@@ -1,22 +1,24 @@
 #ifndef VETERINARIA_H_INCLUDED
 #define VETERINARIA_H_INCLUDED
 
-void Veterinaria();
-//void escribir(struct dogType *a);
-long HashFunction(char* nombre);
-
-struct dogType
-{
-    long ID;
+struct dogType{
     char nombre[32];
     char tipo[32];
     long edad;
     char raza[16];
     long estatura;
     float peso;
-    char sexo[1];
-    int initialized;
+    char sexo[2];
+    short initialized;
+    long next;
+    long prev;
 };
+void Veterinaria();
+void escribir(struct dogType *a);
+long HashFunction(char* nombre);
+
+
+
 
 
 
