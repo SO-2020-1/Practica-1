25523ddeaf691e317d5aac33d9057799e90accf3
esgonzalezca <36159488+esgonzalezca@users.noreply.github.com>
2020-03-31 19:41:40 -0500

    Update veterinaria.h

--- veterinaria.h
+++ veterinaria.h
@@ -1,7 +1,9 @@
 #ifndef VETERINARIA_H_INCLUDED
 #define VETERINARIA_H_INCLUDED
+#define TamanoVeterinaria 1000000L
 
 struct dogType{
+    long id;
     char nombre[32];
     char tipo[32];
     long edad;
