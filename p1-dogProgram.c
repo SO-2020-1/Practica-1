43b703bb4e6df9db57c85083408b0e0fb3f0f2db
esgonzalezca <36159488+esgonzalezca@users.noreply.github.com>
2020-03-31 19:40:52 -0500

    Update p1-dogProgram.c

--- p1-dogProgram.c
+++ p1-dogProgram.c
@@ -8,84 +8,122 @@
 void makeRegister(){     //Opcion 1
 
 
-      
-    struct dogType perro;
-    char nombre[32];
-     printw("Ingrese el nombre del perro \n ");
-     refresh();
-     scanf("%32s",nombre);
-     strcpy(perro.nombre, nombre);
+	  
+	struct dogType perro;
+	char nombre[32];
+	 printw("Ingrese el nombre del perro \n ");
+	 refresh();
+	 scanw("%32s",nombre);
+	 strcpy(perro.nombre, nombre);
    
-    char tipo[32];
-     printw("Ingrese el tipo del perro \n ");
-     refresh();
-     scanf("%32s",tipo);
-     
+	char tipo[32];
+	 printw("Ingrese el tipo del perro \n ");
+	 refresh();
+	 scanw("%32s",tipo);
+	 
    strcpy( perro.tipo,tipo);
-    int edad;
-     printw("Ingrese la edad del perro \n ");
-     refresh();
-     scanf("%d",&edad);
-    perro.edad=edad;
-    char raza[16];
-     printw("Ingrese la raza del perro \n");
-     refresh();
-     scanf("%16s",raza);
-    strcpy(perro.raza,raza);
-    long estatura;
-     printw("Ingrese la estatura del canchoso \n");
-     refresh();
-     scanf("%ld",&estatura);
-    perro.estatura=estatura;
+	int edad;
+	 printw("Ingrese la edad del perro \n ");
+	 refresh();
+	 scanw("%d",&edad);
+	perro.edad=edad;
+	char raza[16];
+	 printw("Ingrese la raza del perro \n");
+	 refresh();
+	 scanw("%16s",raza);
+	strcpy(perro.raza,raza);
+	long estatura;
+	 printw("Ingrese la estatura del canchoso \n");
+	 refresh();
+	 scanw("%ld",&estatura);
+	perro.estatura=estatura;
   float peso;
-     printw("Ingrese el peso del perro \n");
-     refresh();
-     scanf("%f",&peso);
-    perro.peso=peso;
+	 printw("Ingrese el peso del perro \n");
+	 refresh();
+	 scanw("%f",&peso);
+	perro.peso=peso;
 
-     
-    char sexo[2];
-     printw("Ingrese 'M' si es macho o 'F' si es hembra \n");
-     refresh();
-     scanf("%2s",sexo);
-     strcpy(perro.sexo,sexo);
+	 
+	char sexo[2];
+	 printw("Ingrese 'M' si es macho o 'F' si es hembra \n");
+	 refresh();
+	 scanw("%2s",sexo);
+	 strcpy(perro.sexo,sexo);
 
-     //vamos a abrir el archivo en moco reemplazar 
+	 //vamos a abrir el archivo en moco reemplazar 
 
 
 long hashNumber=HashFunction(perro.nombre);
+/*char nombrePrueba[32] ="Abanero";
 
-//la estructura pesa    104 bytes
+long hashPropio=HashFunction("Hernán");
+long hashPropio2=HashFunction("Poter");
+printw(" %ld %s %ld", hashPropio," ", hashPropio2, " \n");
+refresh();*/
+//la estructura pesa    120 bytes
 //vamos a esa posición en el archivo y le buscamos el dato initialized
 
-    short initiliazed;
-   
-    printw("%ld",hashNumber, " \n");
-   refresh();
-   FILE *fp = fopen("dataDogs.dat", "rb+");
-       fseek(fp, (104*hashNumber)+94, SEEK_SET);
+	short initiliazed;
+   struct  dogType perroCopia;
    
    
-   
-   
-    fread(&initiliazed, 2, 1, fp);
-    printw("%ld",initiliazed, " \n");
-     refresh();
-if(initiliazed==0){
-fseek(fp, 0, SEEK_SET);
-fseek(fp, (104*hashNumber), SEEK_SET);
-perro.initialized=1;
- fwrite(&perro, 104, 1, fp);
-
-}
-
-   
-    fclose(fp);
     
+   
+   FILE *fp = fopen("dataDogs.dat", "rb+");
+	   fseek(fp, (sizeof(perro)*hashNumber), SEEK_SET);
+   
+   
+   
+   
+	fread(&perroCopia, sizeof(perro), 1, fp);
+	printw("%d %s",perroCopia.initialized ,perroCopia.nombre, " \n");
+	 refresh();
+	if(perroCopia.initialized==0){
+		fseek(fp, 0, SEEK_SET);
+		fseek(fp, (sizeof(perro)*hashNumber), SEEK_SET);
+		perro.initialized=1;
+ 		fwrite(&perro, sizeof(perro), 1, fp);
+
+	}else
+	{
+		while(perroCopia.next!=-1){
+			long siguiente= perroCopia.next;
+			fseek(fp, 0, SEEK_SET);
+			fseek(fp, (sizeof(perro)*siguiente), SEEK_SET);
+			fread(&perroCopia, sizeof(perro), 1, fp);
+			if(perroCopia.next==-1){
+				perro.prev=perroCopia.id;
+				perro.next=-1;
+				break;
+
+			}
+
+
+		}
+		while(perroCopia.initialized!=0){
+			fread(&perroCopia, sizeof(perro), 1, fp);
+		
+		}
+
+		
+		fseek(fp, (sizeof(perro)*perroCopia.), SEEK_SET);
+		perro.initialized=1;
+ 		fwrite(&perro, sizeof(perro), 1, fp);
+
+
+
+
+
+	}
+	
+
+   
+	fclose(fp);
+	
 }
 
 void showRegister(){    //Opcion 2
-    
+	
 }
 
 void deleteRegister(){  //Opcion 3
@@ -100,64 +138,64 @@ void seekRegister(){    //Opcion 4
 
 void MENU(){            //Funcion menu ciclica que sera ejecutada en main()
 
-    initscr();          //Inicio la capacidad de utilizar funciones de curses.h
-    
-    int opc = 0;        //Declaracion de variable para elegir opcion
-    int aux = 0;        //Variable para salir del ciclo
-    
-    do{
-        
-        clear();        //limpio pantalla
-       
-                        //Opciones, se usa printw en vez de printf para manejo de pantalla
+	initscr();          //Inicio la capacidad de utilizar funciones de curses.h
+	
+	int opc = 0;        //Declaracion de variable para elegir opcion
+	int aux = 0;        //Variable para salir del ciclo
+	
+	do{
+		
+		clear();        //limpio pantalla
+	   
+						//Opciones, se usa printw en vez de printf para manejo de pantalla
 
-        printw("MENU\n\n");
-        printw("1.Ingresar registro.\n");
-        printw("2.Ver registro.\n");
-        printw("3.Borrar registro.\n");
-        printw("4.Buscar registro.\n");
-        printw("5.SALIR.\n");
-        refresh();      //Permite mostrar todos los printw
-        scanf("%d",&opc);//pido la opcion deseada por el usuario
+		printw("MENU\n\n");
+		printw("1.Ingresar registro.\n");
+		printw("2.Ver registro.\n");
+		printw("3.Borrar registro.\n");
+		printw("4.Buscar registro.\n");
+		printw("5.SALIR.\n");
+		refresh();      //Permite mostrar todos los printw
+		scanw("%d",&opc);//pido la opcion deseada por el usuario
 
-        //Segun la opcion se va por el caso correspondiente
-        switch(opc){
-            case 1:{
-                makeRegister();
-               
-                getch();
-                clear();
-                break;
-            }
-            case 2:{
-                showRegister();
-                printw("Presione cualquier tecla para continuar..\n");
-                getch();
-                clear();
-                break;
-            }
-            case 3:{
-                deleteRegister();
-                printw("Presione cualquier tecla para continuar..\n");
-                getch();
-                clear();
-                break;
-            }
-            case 4:{
-                seekRegister();
-                printw("Presione cualquier tecla para continuar..\n");
-                getch();    
-                clear();
-                break;
-            }
-            case 5:{
-                aux=-1;
-            }
-            
+		//Segun la opcion se va por el caso correspondiente
+		switch(opc){
+			case 1:{
+				makeRegister();
+			   
+				getch();
+				clear();
+				break;
+			}
+			case 2:{
+				showRegister();
+				printw("Presione cualquier tecla para continuar..\n");
+				getch();
+				clear();
+				break;
+			}
+			case 3:{
+				deleteRegister();
+				printw("Presione cualquier tecla para continuar..\n");
+				getch();
+				clear();
+				break;
+			}
+			case 4:{
+				seekRegister();
+				printw("Presione cualquier tecla para continuar..\n");
+				getch();    
+				clear();
+				break;
+			}
+			case 5:{
+				aux=-1;
+			}
+			
 
-        };
+		};
 
-    }while(aux!=-1);
+	}while(aux!=-1);
 
 
 
@@ -168,13 +206,13 @@ void MENU(){            //Funcion menu ciclica que sera ejecutada en main()
 endwin(); //Termino el manejo de pantalla
 
 
-    
+	
 }
 
 
 int main(){
-    
-    MENU();
-    return 0;
+	//veterinaria();
+	MENU();
+	return 0;
 
 }
