214f06890dd34d3909f4d0aad8956b7db066bf7f
esgonzalezca <36159488+esgonzalezca@users.noreply.github.com>
2020-03-31 19:42:15 -0500

    Update veterinaria.c

--- veterinaria.c
+++ veterinaria.c
@@ -5,11 +5,11 @@
 #include "locale.h"
 #include "math.h"
 #include "veterinaria.h"
-#define TamanoVeterinaria 1000000L
 
 
 
-struct dogType Veterinaria[(2*TamanoVeterinaria)];
+
+struct dogType Veterinaria[((unsigned long)(1.9*TamanoVeterinaria))];
 
 long HashFunction(char* nombre){
     long ascii=0;
@@ -17,7 +17,7 @@ long HashFunction(char* nombre){
     for(long long currentChar = 0 ; currentChar < strlen(nombre) ; currentChar++){
         unsigned long long _p = powl(a,currentChar);
         ascii += ((unsigned int)(nombre[currentChar]))*(unsigned long long)_p;
-        ascii %= (unsigned long)2*TamanoVeterinaria;
+        ascii %= (unsigned long)(1.9*TamanoVeterinaria);
     }
     
     return ascii;
@@ -26,7 +26,7 @@ long HashFunction(char* nombre){
 void escribir(struct dogType *a){
     FILE *fp;
 	fp = fopen ( "dataDogs.dat", "wb" );
-    fwrite(a, sizeof(struct dogType),2*TamanoVeterinaria,fp);
+    fwrite(a, sizeof(struct dogType),(unsigned long)(1.9*TamanoVeterinaria),fp);
     fclose ( fp );
  	return;
 }
@@ -37,33 +37,43 @@ void veterinaria(){
     fpnombres = fopen("nombres.txt","r");
     bool colision = true;
     char nombre[32];
-    for(long x = 0; x<(2*TamanoVeterinaria);x++){
+    for(long x = 0; x<((unsigned long)(1.9*TamanoVeterinaria));x++){
         Veterinaria[x].initialized = 0;
         Veterinaria[x].next = -1;
         Veterinaria[x].prev = -1;
+        Veterinaria[x].id=x;
     }
     for(long i = 0; i < TamanoVeterinaria ; i++){
         colision = true;
         if(i%1700 == 0)fseek(fpnombres, 0, SEEK_SET);
         fgets(nombre,sizeof(nombre),fpnombres);
+        strtok(nombre, "\n");
         long index = HashFunction(nombre);
+        //printf ("%s %s %s %ld  \n"," el nombre: ",nombre, " tiene el hash :", index);
         while(Veterinaria[index].next != -1){
             index = Veterinaria[index].next;
         }
         unsigned long previous = index;
         do{
             if(Veterinaria[index].initialized == 1){
-            index = (index+1)%(2*TamanoVeterinaria);
-            //printf("colision!!\n");
+           //printf("%s %s %s ", Veterinaria[index].nombre ,"colision!!\n ", nombre);
+            
+            index = (index+1)%((unsigned long)(1.9*TamanoVeterinaria));
+          
+            
             }
             else colision=false;
         }while(colision);
         Veterinaria[index].initialized=1;
+        
+        
+      //  printf("%ld \n", index);
         if(previous != index){
             Veterinaria[index].prev = previous;
             Veterinaria[previous].next = index;
         }
         strcpy(Veterinaria[index].nombre, nombre);
+       // printf ("%s %s %s %ld  \n"," el nombre: ",nombre, " Fue ingresado en la posicion :", index);
         strcpy(Veterinaria[index].tipo, "perro");
         Veterinaria[index].edad=2*i % 14;
         strcpy(Veterinaria[index].raza, "Golden");
@@ -71,11 +81,15 @@ void veterinaria(){
         Veterinaria[index].peso = (2.3 * i + i % 50) + 10;
         if(i %2 == 0) strcpy(Veterinaria[index].sexo, "H");
         else strcpy(Veterinaria[index].sexo, "M");
-        printf("%li\n",i);
+       // printf("%li\n",i);
         //printf("index: %li , %i \n",index,Veterinaria[index].initialized);
+         //printf ("%s \n", Veterinaria[index].nombre);
     }
     //for(long x = 0; x<(TamanoVeterinaria);x++)printf("%i",Veterinaria[x].initialized);
     
+    //printf ("%s %ld","el hash de Doro teo es: ",HashFunction("Doroteo"));
+    //printf ("%s %ld","El hash de Hommer es: ",HashFunction("Hommer"));
+
     escribir(Veterinaria);
     fclose(fpnombres);
     
